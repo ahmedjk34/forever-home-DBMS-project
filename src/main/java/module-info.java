1bc545b64a4f1815cc44d45e23d2b00110cad5a8
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-28 17:27:10 +0200

    Added java.desktop for Swing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.mycompany.foreverhomedbmsproject {
     requires javafx.fxml;
     requires java.base;
     requires java.sql;
+    requires java.desktop; // Corrected to require java.desktop for Swing
 
     opens com.mycompany.foreverhomedbmsproject to javafx.fxml;
     exports com.mycompany.foreverhomedbmsproject;
