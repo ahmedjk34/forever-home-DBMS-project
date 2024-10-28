8047f7b21fc9c7694477956d38f2dd81f766ce34
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-28 16:19:53 +0200

    Person class created with setters and getters

---
+++ src/main/java/com/mycompany/foreverhomedbmsproject/Server/Person.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.foreverhomedbmsproject.Server;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String ssn;
+    private String password;
+    private String fName;
+    private String lName;
+    private String address;
+    private String socialStatus;
+    private String email;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+
+    // Constructor
+    public Person(String ssn, String password, String fName, String lName, String address,
+                  String socialStatus, String email, String phoneNumber, LocalDate dateOfBirth) {
+        this.ssn = ssn;
+        this.password = password;
+        this.fName = fName;
+        this.lName = lName;
+        this.address = address;
+        this.socialStatus = socialStatus;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // Setters and Getters
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSocialStatus() {
+        return socialStatus;
+    }
+
+    public void setSocialStatus(String socialStatus) {
+        this.socialStatus = socialStatus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    //getting the full name
+    public String getFullName(){
+        return fName + " " + lName;
+    }
+    
+    // toString() Method
+    @Override
+    public String toString() {
+        return "Person{" +
+                "ssn='" + ssn + '\'' +
+                ", password='" + password + '\'' +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", address='" + address + '\'' +
+                ", socialStatus='" + socialStatus + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
