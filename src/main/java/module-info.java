a8e234f3bc9ab8399e09104eaf03cd652f2e1735
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-28 17:10:22 +0200

    Added JDBC

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.foreverhomedbmsproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.mycompany.foreverhomedbmsproject to javafx.fxml;
     exports com.mycompany.foreverhomedbmsproject;
