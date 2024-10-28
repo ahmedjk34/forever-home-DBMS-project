1a728dec88c73ce621fe3882c5d46b5bf043827a
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-28 13:36:51 +0200

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.foreverhomedbmsproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.foreverhomedbmsproject to javafx.fxml;
     exports com.mycompany.foreverhomedbmsproject;
