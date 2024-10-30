de47f6cedd4e4b53dd1a5bf9a493ab35571b9bf3
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-30 15:57:24 +0200

    Created Adopter class

---
+++ src/main/java/com/mycompany/foreverhomedbmsproject/Server/Adopter.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.foreverhomedbmsproject.Server;
+
+import java.time.LocalDate;
+
+public class Adopter extends Person {
+
+    private String occupation;
+    private int numberOfPetsOwned;
+    private int numberOfChildren;
+    private double yearlyIncome;
+
+    // Constructor
+    public Adopter(String ssn, String password, String fName, String lName, String address,
+                   String socialStatus, String email, String phoneNumber, LocalDate dateOfBirth,
+                   String occupation, int numberOfPetsOwned, int numberOfChildren, double yearlyIncome) {
+        super(ssn, password, fName, lName, address, socialStatus, email, phoneNumber, dateOfBirth);
+        this.occupation = occupation;
+        this.numberOfPetsOwned = numberOfPetsOwned;
+        this.numberOfChildren = numberOfChildren;
+        this.yearlyIncome = yearlyIncome;
+    }
+
+    // Getters and Setters
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public int getNumberOfPetsOwned() {
+        return numberOfPetsOwned;
+    }
+
+    public void setNumberOfPetsOwned(int numberOfPetsOwned) {
+        this.numberOfPetsOwned = numberOfPetsOwned;
+    }
+
+    public int getNumberOfChildren() {
+        return numberOfChildren;
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+    }
+
+    public double getYearlyIncome() {
+        return yearlyIncome;
+    }
+
+    public void setYearlyIncome(double yearlyIncome) {
+        this.yearlyIncome = yearlyIncome;
+    }
+
+    // toString() Method
+    @Override
+    public String toString() {
+        return "Adopter{" +
+                "occupation='" + occupation + '\'' +
+                ", numberOfPetsOwned=" + numberOfPetsOwned +
+                ", numberOfChildren=" + numberOfChildren +
+                ", yearlyIncome=" + yearlyIncome +
+                ", fullName='" + getFullName() + '\'' +
+                "} " + super.toString();
+    }
+}
