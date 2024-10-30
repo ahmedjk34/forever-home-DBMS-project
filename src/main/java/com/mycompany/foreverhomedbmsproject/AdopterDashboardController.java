46e7384380d0bca6e97677c543740c4a0af97a1a
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-30 16:36:32 +0200

    Added the method to set lables after logging

--- src/main/java/com/mycompany/foreverhomedbmsproject/AdopterDashboardController.java
+++ src/main/java/com/mycompany/foreverhomedbmsproject/AdopterDashboardController.java
@@ -4,23 +4,68 @@
  */
 package com.mycompany.foreverhomedbmsproject;
 
+import com.mycompany.foreverhomedbmsproject.Server.Adopter;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author lenovo
- */
 public class AdopterDashboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    private Adopter adopter;
+
+    @FXML
+    private Label ssnLabel;
+    @FXML
+    private Label fullNameLabel;
+    @FXML
+    private Label genderLabel;
+    @FXML
+    private Label phoneNumberLabel;
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private Label socialStatusLabel;
+    @FXML
+    private Label addressLabel;
+    @FXML
+    private Label occupationLabel;
+    @FXML
+    private Label numberOfPetsLabel;
+    @FXML
+    private Label numberOfChildrenLabel;
+    @FXML
+    private Label yearlyIncomeLabel;
+    @FXML
+    private Label dobLabel;
+    @FXML
+    private Label ageLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // Leave empty if you plan to populate fields after setting adopter
     }    
-    
+
+    public void setAdopter(Adopter adopter) {
+        this.adopter = adopter;
+        updateFields();
+    }
+
+    private void updateFields() {
+        ssnLabel.setText(adopter.getSsn());
+        fullNameLabel.setText(adopter.getFullName());
+        genderLabel.setText(adopter.getGender());
+        phoneNumberLabel.setText(adopter.getPhoneNumber());
+        emailLabel.setText(adopter.getEmail());
+        socialStatusLabel.setText(adopter.getSocialStatus());
+        addressLabel.setText(adopter.getAddress());
+        occupationLabel.setText(adopter.getOccupation());
+        numberOfPetsLabel.setText(String.valueOf(adopter.getNumberOfPetsOwned()));
+        numberOfChildrenLabel.setText(String.valueOf(adopter.getNumberOfChildren()));
+        yearlyIncomeLabel.setText(String.format("$%.2f", adopter.getYearlyIncome()));
+        dobLabel.setText(adopter.getDateOfBirth().toString());
+        ageLabel.setText(String.valueOf(adopter.getAge())); 
+    }
 }
