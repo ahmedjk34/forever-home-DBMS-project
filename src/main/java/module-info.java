be8b6d3cee6ddaae4f97459162b5025f3e7a2fdc
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-11-01 12:27:17 +0200

    Added jasper report library + placeholder report

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,12 @@ module com.mycompany.foreverhomedbmsproject {
     requires java.base;
     requires java.sql;
     requires java.desktop;
+    requires net.sf.jasperreports.core;
+
+    // Open packages for JavaFX reflection
     opens com.mycompany.foreverhomedbmsproject.Server to javafx.base;
     opens com.mycompany.foreverhomedbmsproject to javafx.fxml;
+
+    // Export the main package
     exports com.mycompany.foreverhomedbmsproject;
 }
