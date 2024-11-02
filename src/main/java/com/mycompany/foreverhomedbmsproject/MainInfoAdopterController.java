d98acefabbd238e8cb7ff0f5c28e32d814fa7276
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-11-02 19:42:44 +0200

    Changed lables to textfields

--- src/main/java/com/mycompany/foreverhomedbmsproject/MainInfoAdopterController.java
+++ src/main/java/com/mycompany/foreverhomedbmsproject/MainInfoAdopterController.java
@@ -5,14 +5,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainInfoAdopterController implements Initializable {
 
     private Adopter adopter;
 
     @FXML
-    private Label ssnLabel, fullNameLabel, genderLabel, phoneNumberLabel, emailLabel, socialStatusLabel, addressLabel, occupationLabel, numberOfPetsLabel, numberOfChildrenLabel, yearlyIncomeLabel, dobLabel, ageLabel;
+    private TextField ssnTextField, fullNameTextField, genderTextField, phoneNumberTextField, emailTextField, socialStatusTextField, addressTextField, occupationTextField, numberOfPetsTextField, numberOfChildrenTextField, yearlyIncomeTextField, dobTextField, ageTextField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -26,19 +26,19 @@ public class MainInfoAdopterController implements Initializable {
 
     private void updateFields() {
         if (adopter != null) {
-            ssnLabel.setText(adopter.getSsn());
-            fullNameLabel.setText(adopter.getFullName());
-            genderLabel.setText(adopter.getGender());
-            phoneNumberLabel.setText(adopter.getPhoneNumber());
-            emailLabel.setText(adopter.getEmail());
-            socialStatusLabel.setText(adopter.getSocialStatus());
-            addressLabel.setText(adopter.getAddress());
-            occupationLabel.setText(adopter.getOccupation());
-            numberOfPetsLabel.setText(String.valueOf(adopter.getNumberOfPetsOwned()));
-            numberOfChildrenLabel.setText(String.valueOf(adopter.getNumberOfChildren()));
-            yearlyIncomeLabel.setText(String.format("$%.2f", adopter.getYearlyIncome()));
-            dobLabel.setText(adopter.getDateOfBirth().toString());
-            ageLabel.setText(String.valueOf(adopter.getAge())); 
+            ssnTextField.setText(adopter.getSsn());
+            fullNameTextField.setText(adopter.getFullName());
+            genderTextField.setText(adopter.getGender());
+            phoneNumberTextField.setText(adopter.getPhoneNumber());
+            emailTextField.setText(adopter.getEmail());
+            socialStatusTextField.setText(adopter.getSocialStatus());
+            addressTextField.setText(adopter.getAddress());
+            occupationTextField.setText(adopter.getOccupation());
+            numberOfPetsTextField.setText(String.valueOf(adopter.getNumberOfPetsOwned()));
+            numberOfChildrenTextField.setText(String.valueOf(adopter.getNumberOfChildren()));
+            yearlyIncomeTextField.setText(String.format("$%.2f", adopter.getYearlyIncome()));
+            dobTextField.setText(adopter.getDateOfBirth().toString());
+            ageTextField.setText(String.valueOf(adopter.getAge())); 
         }
     }
 }
