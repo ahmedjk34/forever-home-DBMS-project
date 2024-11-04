8b75751a095cee15e1934236ac79a71b00c87d63
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-11-04 14:12:02 +0200

    Window size changes

--- src/main/java/com/mycompany/foreverhomedbmsproject/AuthenticationController.java
+++ src/main/java/com/mycompany/foreverhomedbmsproject/AuthenticationController.java
@@ -102,7 +102,7 @@ private void handleLogin() throws IOException {
                 AdopterDashboardController controller = loader.getController();
                 controller.setAdopter(loggedAdopter); 
                 Stage stage = (Stage) ((Node) ssnField).getScene().getWindow();
-                stage.setWidth(840);
+                stage.setWidth(930);
                 stage.setHeight(685);
                 stage.getScene().setRoot(root);
 
