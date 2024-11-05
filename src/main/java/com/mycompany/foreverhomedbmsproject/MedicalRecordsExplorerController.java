fe8623b201b2b83d0df66abceb0c6a435c5e305e
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-11-05 13:03:22 +0200

    Added remove & add button methods to open the popups

--- src/main/java/com/mycompany/foreverhomedbmsproject/MedicalRecordsExplorerController.java
+++ src/main/java/com/mycompany/foreverhomedbmsproject/MedicalRecordsExplorerController.java
@@ -1,5 +1,7 @@
 package com.mycompany.foreverhomedbmsproject;
 
+import com.mycompany.foreverhomedbmsproject.Popups.NewMedicalRecordPopupController;
+import com.mycompany.foreverhomedbmsproject.Popups.RemoveMedicalRecordPopupController;
 import com.mycompany.foreverhomedbmsproject.Server.MedicalRecord;
 import java.io.IOException;
 import java.net.URL;
@@ -11,12 +13,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MedicalRecordsExplorerController implements Initializable {
 
@@ -42,80 +50,78 @@ public class MedicalRecordsExplorerController implements Initializable {
     }
 
     private void getMedicalRecords() {
-    String query = "SELECT m.Record_ID, a.Animal_ID, a.animal_image ,m.Clinic_Name, a.name, a.gender, " +
-                   "vr.Vaccination, tr.Treatment, ir.Illness, nr.Note, " +
-                   "DATE_PART('year', AGE(a.Date_of_Birth))::text AS Age " +
-                   "FROM Medical_Record m " +
-                   "JOIN Animal a ON m.Animal_ID = a.Animal_ID " +
-                   "LEFT JOIN Vaccination_Record vr ON m.Record_ID = vr.Record_ID " +
-                   "LEFT JOIN Treatment_Record tr ON m.Record_ID = tr.Record_ID " +
-                   "LEFT JOIN Illness_Record ir ON m.Record_ID = ir.Record_ID " +
-                   "LEFT JOIN Note_Record nr ON m.Record_ID = nr.Record_ID " +
-                   "WHERE a.Animal_ID = m.Animal_ID";
 
+        medicalRecordsList.clear();
 
+        String query = "SELECT m.Record_ID, a.Animal_ID, a.animal_image ,m.Clinic_Name, a.name, a.gender, "
+                + "vr.Vaccination, tr.Treatment, ir.Illness, nr.Note, "
+                + "DATE_PART('year', AGE(a.Date_of_Birth))::text AS Age "
+                + "FROM Medical_Record m "
+                + "JOIN Animal a ON m.Animal_ID = a.Animal_ID "
+                + "LEFT JOIN Vaccination_Record vr ON m.Record_ID = vr.Record_ID "
+                + "LEFT JOIN Treatment_Record tr ON m.Record_ID = tr.Record_ID "
+                + "LEFT JOIN Illness_Record ir ON m.Record_ID = ir.Record_ID "
+                + "LEFT JOIN Note_Record nr ON m.Record_ID = nr.Record_ID "
+                + "WHERE a.Animal_ID = m.Animal_ID";
 
-    try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-         PreparedStatement stmt = conn.prepareStatement(query)) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); PreparedStatement stmt = conn.prepareStatement(query)) {
 
 //        stmt.setInt(1, specificAnimalId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                MedicalRecord currentRecord = null;
 
-        try (ResultSet rs = stmt.executeQuery()) {
-            MedicalRecord currentRecord = null;
+                while (rs.next()) {
+                    int recordId = rs.getInt("Record_ID");
+                    int animalId = rs.getInt("Animal_ID");
+                    String clinicName = rs.getString("Clinic_Name");
+                    String animalName = rs.getString("name");
+                    String animalGender = rs.getString("gender");
+                    String animalAge = rs.getString("Age");
+                    String animalImage = rs.getString("animal_image");
 
-            while (rs.next()) {
-                int recordId = rs.getInt("Record_ID");
-                int animalId = rs.getInt("Animal_ID");
-                String clinicName = rs.getString("Clinic_Name");
-                String animalName = rs.getString("name");
-                String animalGender = rs.getString("gender");
-                String animalAge = rs.getString("Age");
-                String animalImage = rs.getString("animal_image");
+                    // Check if we are processing a new medical record
+                    if (currentRecord == null || currentRecord.getRecordId() != recordId) {
+                        // Create a new MedicalRecord instance and add it to the list
+                        currentRecord = new MedicalRecord(recordId, animalId, clinicName, animalName, animalGender, animalAge, animalImage);
+                        medicalRecordsList.add(currentRecord);
+                    }
 
-                // Check if we are processing a new medical record
-                if (currentRecord == null || currentRecord.getRecordId() != recordId) {
-                    // Create a new MedicalRecord instance and add it to the list
-                    currentRecord = new MedicalRecord(recordId, animalId, clinicName , animalName , animalGender , animalAge , animalImage);
-                    medicalRecordsList.add(currentRecord);
-                }
+                    // Add vaccination if it's not already in the list
+                    String vaccination = rs.getString("Vaccination");
+                    if (vaccination != null && currentRecord.getVaccinationRecords().stream()
+                            .noneMatch(v -> v.getVaccination().equals(vaccination))) {
+                        currentRecord.addVaccinationRecord(vaccination);
+                    }
 
-                // Add vaccination if it's not already in the list
-                String vaccination = rs.getString("Vaccination");
-                if (vaccination != null && currentRecord.getVaccinationRecords().stream()
-                        .noneMatch(v -> v.getVaccination().equals(vaccination))) {
-                    currentRecord.addVaccinationRecord(vaccination);
-                }
+                    // Add treatment if it's not already in the list
+                    String treatment = rs.getString("Treatment");
+                    if (treatment != null && currentRecord.getTreatmentRecords().stream()
+                            .noneMatch(t -> t.getTreatment().equals(treatment))) {
+                        currentRecord.addTreatmentRecord(treatment);
+                    }
 
-                // Add treatment if it's not already in the list
-                String treatment = rs.getString("Treatment");
-                if (treatment != null && currentRecord.getTreatmentRecords().stream()
-                        .noneMatch(t -> t.getTreatment().equals(treatment))) {
-                    currentRecord.addTreatmentRecord(treatment);
-                }
+                    // Add illness if it's not already in the list
+                    String illness = rs.getString("Illness");
+                    if (illness != null && currentRecord.getIllnessRecords().stream()
+                            .noneMatch(i -> i.getIllness().equals(illness))) {
+                        currentRecord.addIllnessRecord(illness);
+                    }
 
-                // Add illness if it's not already in the list
-                String illness = rs.getString("Illness");
-                if (illness != null && currentRecord.getIllnessRecords().stream()
-                        .noneMatch(i -> i.getIllness().equals(illness))) {
-                    currentRecord.addIllnessRecord(illness);
-                }
-
-                // Add note if it's not already in the list
-                String note = rs.getString("Note");
-                if (note != null && currentRecord.getNoteRecords().stream()
-                        .noneMatch(n -> n.getNote().equals(note))) {
-                    currentRecord.addNoteRecord(note);
+                    // Add note if it's not already in the list
+                    String note = rs.getString("Note");
+                    if (note != null && currentRecord.getNoteRecords().stream()
+                            .noneMatch(n -> n.getNote().equals(note))) {
+                        currentRecord.addNoteRecord(note);
+                    }
                 }
             }
+
+            displayMedicalRecords();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-    displayMedicalRecords();
-
-    } catch (SQLException e) {
-        e.printStackTrace();
     }
-}
-
 
     private void displayMedicalRecords() {
         for (MedicalRecord record : medicalRecordsList) {
@@ -127,12 +133,71 @@ public class MedicalRecordsExplorerController implements Initializable {
                 itemController.setRecordData(record);
 
                 recordsContainer.getChildren().add(recordNode);
-                
-         
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    @FXML
+    private void addMedicalRecordClick() {
+        try {
+            // Load the FXML for the popup
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Popups/NewMedicalRecordPopup.fxml"));
+            AnchorPane popup = loader.load();
+            NewMedicalRecordPopupController controller = loader.getController();
+
+            // Create a new stage for the popup
+            Stage popupStage = new Stage();
+            popupStage.setTitle("Add a New Medical Record");
+
+            // Set the scene with the loaded FXML
+            Scene scene = new Scene(popup);
+            popupStage.setScene(scene);
+
+            // Optional: Set the modality to block input to other windows
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+
+            // Show the popup and wait for it to close
+            popupStage.showAndWait();
+
+            recordsContainer.getChildren().clear();
+
+            getMedicalRecords();
+        } catch (IOException ex) {
+            Logger.getLogger(MedicalRecordsExplorerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @FXML
+    private void deleteMedicalRecordClick() {
+        try {
+            // Load the FXML for the popup
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Popups/RemoveMedicalRecordPopup.fxml"));
+            AnchorPane popup = loader.load();
+            RemoveMedicalRecordPopupController controller = loader.getController();
+
+            // Create a new stage for the popup
+            Stage popupStage = new Stage();
+            popupStage.setTitle("Delete a Medical Record");
+
+            // Set the scene with the loaded FXML
+            Scene scene = new Scene(popup);
+            popupStage.setScene(scene);
+
+            // Optional: Set the modality to block input to other windows
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+
+            // Show the popup and wait for it to close
+            popupStage.showAndWait();
+
+            recordsContainer.getChildren().clear();
+            getMedicalRecords();
+        } catch (IOException ex) {
+            Logger.getLogger(MedicalRecordsExplorerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
