5d704a8c032814348485eaaefbcb12fc58be89c2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-11-05 16:17:58 +0200

    Created NewDonationPopup

---
+++ src/main/java/com/mycompany/foreverhomedbmsproject/Popups/NewDonationPopupController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.foreverhomedbmsproject.Popups;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author lenovo
+ */
+public class NewDonationPopupController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    private int eventId;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+}
